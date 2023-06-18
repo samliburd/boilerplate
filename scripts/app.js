d0534e60f741b4673143249e11433389ae5ac18c
samliburd <liburdsam@gmail.com>
2023-06-18 19:28:45 +0100

    Updated app.js again (again) to properly space text

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,27 @@
-document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-    
-    // Get values from input boxes
+const getFormValues = () => {
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
-    const capitalised = name.charAt(0).toUpperCase() + name.slice(1);
-    const generatePassword = () => {
-        const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        const passwordLength = 14;
-        let password = "";
-        for (let i = 0; i <= passwordLength; i++) {
-            let randomNumber = Math.floor(Math.random() * chars.length);
-            password += chars.substring(randomNumber, randomNumber + 1);
-        }
-        return password
+    const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+    return { email, capitalized };
+};
+
+const generatePassword = () => {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 14;
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars[randomNumber];
     }
-    const tempPassword = generatePassword();
-    // Set the text content of the div element
-    document.getElementById("result").innerHTML = `
-Hello ${capitalised},<br>
+    return password;
+};
+
+const setResultText = ({ email, capitalized, tempPassword }) => {
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = `
+Hello ${capitalized},<br>
 <br>
-Apologies for the issues you have been having logging in. I have now updated your account and created a new temporary password. <br>
-<br>
+Apologies for the issues you have been having logging in. I have now updated your account and created a new temporary password.<br>
 Please login on a fresh web browser using the access credentials shown below and amend your password to something more secure.<br>
 <br>
 <strong>Username:</strong> ${email}<br>
@@ -29,26 +29,29 @@ Please login on a fresh web browser using the access credentials shown below and
 <br>
 Kind Regards,<br>
 Web team
-`;
-    document.querySelector(".wrapper").style.display = "flex"
-});
+    `;
+    document.querySelector(".wrapper").style.display = "flex";
+};
 
-// Copy text to clipboard
-document.getElementById("copyButton").addEventListener("click", function () {
-    const resultText = document.getElementById("result").textContent;
-
-    // Create a temporary textarea element
+const copyToClipboard = (text) => {
     const tempTextarea = document.createElement("textarea");
-    tempTextarea.value = resultText;
+    tempTextarea.value = text;
     document.body.appendChild(tempTextarea);
-
-    // Select and copy the text
     tempTextarea.select();
     document.execCommand("copy");
-
-    // Remove the temporary textarea
     document.body.removeChild(tempTextarea);
+};
 
-    // Optionally, provide feedback to the user
-    //alert("Copied to clipboard: " + resultText);
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    const { email, capitalized } = getFormValues();
+    const tempPassword = generatePassword();
+
+    setResultText({ email, capitalized, tempPassword });
+});
+
+document.getElementById("copyButton").addEventListener("click", function () {
+    const resultText = document.getElementById("result").textContent;
+    copyToClipboard(resultText);
 });
